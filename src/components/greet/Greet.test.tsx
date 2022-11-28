1ce46da89583ae5877d7eff1ce57e2a9261dda36
Bahaa <bhaaahmed188@gmail.com>
2022-11-28 18:00:21 +0200

    Describe global function

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-	render(<Greet />);
-	const textElement = screen.getByText("Hello");
-	expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+	test("renders correctly", () => {
+		render(<Greet />);
+		const textElement = screen.getByText("Hello");
+		expect(textElement).toBeInTheDocument();
+	});
 
-test("Greet render with a name", () => {
-	render(<Greet name="Bahaa" />);
-	const textElement = screen.getByText("Hello Bahaa");
-	expect(textElement).toBeInTheDocument();
+	describe("Nesting", () => {
+		test("render with a name", () => {
+			render(<Greet name="Bahaa" />);
+			const textElement = screen.getByText("Hello Bahaa");
+			expect(textElement).toBeInTheDocument();
+		});
+	});
 });
